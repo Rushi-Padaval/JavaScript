c75e384cf443bd4069706fb572578eedf86d0274
Rushi-Padaval <rpadaval7@gmail.com>
2024-01-18 18:36:19 +0530

    minor changes

--- 02_basic/02_arrays_methods.js
+++ 02_basic/02_arrays_methods.js
@@ -1,5 +1,23 @@
 // arrays methods
 
 const MyArr = [0,1,2,3,4,5]
-MyArr.push(6)
+
+// MyArr.push(6)
+// MyArr.pop()
+// console.log(MyArr);
+
+MyArr.unshift(9)
+MyArr.shift()
+
+
+console.log(MyArr.includes(9));
+console.log(MyArr.indexOf(3))
+
+
 console.log(MyArr);
+
+
+
+const newArray = MyArr.join()
+console.log(MyArr);
+console.log(typeof newArray);
