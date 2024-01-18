22387abf95ba1086f8ce67834846722d4c608533
Rushi-Padaval <rpadaval7@gmail.com>
2024-01-18 16:47:52 +0530

    first commit

---
+++ 01_basic/12_datesway.js
@@ -0,0 +1,9 @@
+// let myCreatedDate = new Date("2024-01-01 ")
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myCreatedDate = new Date("2024-01-01")
+
+let myTimestamp = Date.now()
+
+console.log(Math.floor(Date.now()/1000));
