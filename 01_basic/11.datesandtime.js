67c18af5196b0b0cb78b4cea8f5ffee883c50a2f
Rushi-Padaval <rpadaval7@gmail.com>
2024-01-18 16:19:49 +0530

    first commit

---
+++ 01_basic/11.datesandtime.js
@@ -0,0 +1,11 @@
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2024, 0, 1)
+// console.log(myCreatedDate.toString());
+
+
+let myCreatedDate = new Date(2024,1,1, 5,5)
+console.log(myCreatedDate.toLocaleString());
