68a224b18ad1460cedeb818892955de40517055e
Rushi-Padaval <rpadaval7@gmail.com>
2024-01-18 11:39:16 +0530

    first commit

---
+++ 01_basic/10_datesinjs.js
@@ -0,0 +1,9 @@
+// Date
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toJSON());
+console.log(myDate.getHours());
+console.log(myDate.toTimeString());
