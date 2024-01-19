cfe6b33f3f09ebb2a45c92295c93614f0d989cb8
Rushi-Padaval <rpadaval7@gmail.com>
2024-01-19 14:57:08 +0530

    first commit

---
+++ 02_basic/03_arrays.js
@@ -0,0 +1,13 @@
+const marvelHeros = ["Spiderman","Ironman ","AbC"]
+const dcHeros = ["Badman","Joker","Flash"]
+
+// marvelHeros.push(dcHeros)
+// console.log(marvelHeros);
+
+
+
+
+const AllHeros = marvelHeros.concat(dcHeros)
+console.log(`The Heros Of Hollywood is = ${AllHeros}`);
+
+
---
+++ 02_basic/arrayspread.js
@@ -0,0 +1,6 @@
+const marvelHeros = ["Spiderman","Ironman ","AbC"]
+const dcHeros = ["Badman","Joker","Flash"]
+
+
+const AllHeros = [...marvelHeros, ...dcHeros]
+console.log(AllHeros);
