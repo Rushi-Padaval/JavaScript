1e5dddcb0982bf889393d2e2a14948b5a2256bfd
Rushi-Padaval <rpadaval7@gmail.com>
2024-01-19 15:36:56 +0530

    minor changes

--- 02_basic/04_arrayspread.js
+++ 02_basic/04_arrayspread.js
@@ -4,3 +4,20 @@ const dcHeros = ["Badman","Joker","Flash"]
 
 const AllHeros = [...marvelHeros, ...dcHeros]
 console.log(AllHeros);
+
+const realArray = [ [ 1, 2, 2 , [ 6, 7, 8 ],  [ 8,  8,  9]  ]]
+const newArray = realArray.flat(Infinity)
+console.log(newArray);
+
+
+console.log(Array.isArray("rishi"));
+console.log(Array.from("rishi"));
+
+console.log(Array.from({name : "rishi"})) //intersting 
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
