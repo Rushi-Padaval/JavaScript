2323553efb7110219de12958ecfc50678335e924
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-01-20 22:32:18 +0530

    minor changes

--- 02_basic/05_objects.js
+++ 02_basic/05_objects.js
@@ -19,3 +19,10 @@ console.log(user1["email"])
 console.log(user1["full name"]);
 console.log(user1[mySym]);
 console.log(typeof user1);
+
+
+user1.email = "Rushi@outlook.com"
+Object.freeze(user1)
+user1.email  = "rishi@outlook.com"
+
+console.log(user1.email)
