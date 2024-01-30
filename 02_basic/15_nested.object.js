b9320793af447402ed8f3b5b328c669ada71fc96
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-01-30 16:47:28 +0530

    first commit

---
+++ 02_basic/15_nested.object.js
@@ -0,0 +1,13 @@
+const regularuser = {
+    email: "Rishi@gmail.com",
+
+fullname: {
+    userfullname: {
+
+        name: "rishi",
+        lastname: "padaval"
+    }
+}
+}
+
+console.log(regularuser.fullname);
