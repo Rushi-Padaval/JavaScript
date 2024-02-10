16986875efd2763484ed33d88fe7e044bc58ee3e
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-02-10 14:42:14 +0530

    call stack

---
+++ 03_basic/12_callstack.js
@@ -0,0 +1,16 @@
+
+
+// this all code is run the in vs code as well as we tried it on the chrome source snippet for learning of call stack
+
+function one (){
+    console.log("one");
+}
+function two (){
+    console.log("two")
+}
+function three(){
+    console.log("three");
+}
+one()
+two()
+three()
