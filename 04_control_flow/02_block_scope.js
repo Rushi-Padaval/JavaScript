03190c84087f030f22d796e660df837cce51e024
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-02-13 04:22:12 +0530

    if

---
+++ 04_control_flow/02_block_scope.js
@@ -0,0 +1,23 @@
+// const score = 200
+
+// if( score > 100){
+//    const power = "fly"
+//     console.log(`USER POWER IS : ${power}`);
+
+
+// }
+// console.log(`USER POWER IS : ${power}`);
+
+
+const UserIsLoggedIn = true ;
+const UserHaveDebitCard = true ;
+const UserIsLoggedbyGoogle = true ;
+const UserIsLoggedbyEmail = false ;
+
+if (UserIsLoggedIn && UserHaveDebitCard ) {
+    console.log("YOU CAN LOGIN");
+}
+    
+    if (UserIsLoggedbyGoogle || UserIsLoggedbyEmail){
+        console.log("YOU CAN LOGIN");
+    }
