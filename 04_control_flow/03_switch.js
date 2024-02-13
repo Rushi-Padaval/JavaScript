2d6cec3a67f8201ca6ce6d5e826653e893232144
Rushi-Padaval <rushikeshpadaval@outlook.com>
2024-02-13 16:57:33 +0530

    switch case

---
+++ 04_control_flow/03_switch.js
@@ -0,0 +1,29 @@
+
+const month = 4 ;
+
+switch (month) {
+    case  1 :
+        console.log("januvary");
+    
+        
+        break;
+    case  2 :
+        console.log("feb");
+    
+        
+        break;
+    case  3 :
+        console.log("mar");
+    
+        
+        break;
+    case  4 :
+        console.log("april");
+    
+        
+        break;
+
+      default :
+console.log("deafault case match");
+break;
+}
